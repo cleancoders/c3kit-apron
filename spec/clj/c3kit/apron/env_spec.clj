20cddcda544c350591e10d2f1ccf359c0120df4f
slagyr <micahmartin@gmail.com>
2023-07-14 16:23:21 -0700

    adds env

---
+++ spec/clj/c3kit/apron/env_spec.clj
@@ -0,0 +1,54 @@
+(ns c3kit.apron.env-spec
+  (:require
+    [c3kit.apron.env :as sut]
+    [clojure.java.io :as io]
+    [speclj.core :refer :all])
+  (:import (java.io ByteArrayInputStream)))
+
+(describe "env"
+
+  (with-stubs)
+
+  (context "read .env content"
+
+    (it "missing"
+      (let [result (sut/-read-properties "a-file-that-certainly-does-not-exist")]
+        (should= {} result)))
+
+    (it "empty"
+      (with-redefs [io/reader (stub :io/reader {:return (io/reader (ByteArrayInputStream. (.getBytes "")))})]
+        (let [result (sut/-read-properties ".env")]
+          (should= {} result))))
+
+    (it "values"
+      (let [content "foo=bar\nFIZZ=BANG"]
+        (with-redefs [io/reader (stub :io/reader {:return (io/reader (ByteArrayInputStream. (.getBytes content)))})]
+          (let [result (sut/-read-properties ".env")]
+            (should= {"foo" "bar" "FIZZ" "BANG"} result)))))
+    )
+
+  (context "env"
+
+    (after (System/setProperty "PATH" "nil")) ;; PATH is not a typical JVM property, so fear not
+
+    (it "from ENV"
+      (should-not= nil (sut/env "PATH")))
+
+    (it "from System properties"
+      (System/setProperty "PATH" "sys-prop")
+      (should= "sys-prop" (sut/env "PATH")))
+
+    (it "from .env"
+      (System/setProperty "PATH" "sys-prop")
+      (with-redefs [sut/-locals (delay {"PATH" "local"})]
+        (should= "local" (sut/env "PATH"))))
+
+    (it "from override"
+      (System/setProperty "PATH" "sys-prop")
+      (with-redefs [sut/-locals (delay {"PATH" "local"})]
+        (sut/override! "PATH" "override")
+        (should= "override" (sut/env "PATH"))))
+
+    )
+
+  )
---
+++ src/clj/c3kit/apron/env.clj
@@ -0,0 +1,31 @@
+(ns c3kit.apron.env
+  (:require [clojure.java.io :as io])
+  (:import (java.io FileNotFoundException)))
+
+(defn -read-properties [readable]
+  (let [props (java.util.Properties.)]
+    (try
+      (.load props (io/reader readable))
+      (catch FileNotFoundException _))
+    props))
+
+
+(def -overrides (atom {}))
+(def -locals (delay (-read-properties ".env")))
+
+(defn override!
+  "Override the value of any key that may exist in the environment."
+  [key value]
+  (swap! -overrides assoc key value))
+
+(defn env
+  "Resolves the key to the first matching value with the following priority:
+    1) overridden values
+    2) '.env' file (Java Properties format)
+    3) Java System properties
+    4) Environment variables"
+  [key]
+  (or (get @-overrides key)
+      (get @-locals key)
+      (System/getProperty key)
+      (System/getenv key)))
